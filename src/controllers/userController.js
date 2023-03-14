55d10d4f9b32fba97ac48e912ec5b549ab3dd586
brittosmonteiro <brittosmonteiro@gmail.com>
2023-03-14 16:19:25 -0300

    Implementação de leitura do usuário e alteração de senha

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,54 @@
-let users = [];
+import UserModel from "../models/UserModel.js";
 
 export async function readUserController(req, res) {
   const { idUser } = req.params;
+
+  await UserModel.findById(idUser)
+    .then((responseFind) => {
+      if (responseFind) {
+        const userData = {
+          id: responseFind._id.toString(),
+          name: responseFind.name,
+          surname: responseFind.surname,
+          email: responseFind.email,
+          username: responseFind.username,
+        };
+
+        return res.status(200).json({ data: userData });
+      }
+    })
+    .catch((err) => {});
 }
 
-export async function updateUserController(req, res) {
-  const { idUser, data } = req.body;
-  res.send({ data });
+export async function updateUserController(req, res) {}
+
+export async function updatePasswordController(req, res) {
+  const { idUser, updatePass } = req.body;
+  await UserModel.findByIdAndUpdate(idUser, updatePass)
+    .then((responseUpdate) => {
+      if (responseUpdate) {
+        return res.status(200).json({ message: "Password updated" });
+      } else {
+        return;
+      }
+    })
+    .catch((err) => {
+      return;
+    });
 }
 
 export async function deleteUserController(req, res) {
   const { idUser } = req.body;
-  res.send({ idUser });
+
+  await UserModel.findByIdAndDelete(idUser)
+    .then((responseDelete) => {
+      if (responseDelete) {
+        return res.status(200).json({ message: "User deleted" });
+      } else {
+        return;
+      }
+    })
+    .catch((err) => {
+      return;
+    });
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   readUserController,
   updateUserController,
+  updatePasswordController,
   deleteUserController,
 } from "../controllers/userController.js";
 const UserRoute = express.Router();
 
-UserRoute.get("/read/:idUser", readUserController);
+UserRoute.get("/readOne/:idUser", readUserController);
 UserRoute.put("/update", updateUserController);
+UserRoute.put("/updatePassword", updatePasswordController);
 UserRoute.delete("/delete", deleteUserController);
 
 export default UserRoute;
